8e55e7705215f8399806361b1bfaa53e8766c4d7
Aleksandrenko <aleksandrenko@gmail.com>
2016-05-23 01:12:23 +0300

    Sample graphql schema

--- data/schema.js
+++ data/schema.js
@@ -1,6 +1,53 @@
 const typeDefinitions = `
+
+enum Episode { NEWHOPE, EMPIRE, JEDI }
+scalar Url
+
+type Friend {
+  id: ID!
+  name: String
+  homepage: Url
+  friends: [Friend]
+  friend_since: String
+}
+
+type Person {
+ id: ID!
+ name: String
+ homepage: Url
+ friends: [Friend]
+ interests: [Interest]
+}
+
+type Activity {
+  id: ID!
+  name: String!
+  location: Location
+  type: Interest
+}
+
+type Interest {
+  id: ID!
+  name: String!
+}
+
+type Photo {
+  id: ID!
+  name: String!
+  url: Url!
+  location: Location
+}
+
+type Location {
+  id: ID!
+  name: String
+  latlng: String!
+}
+
 type Query {
-  testString: String
+  users: [Person]
+  activities: [Activity]
+  interests: [Interest]
 }
 
 schema {
